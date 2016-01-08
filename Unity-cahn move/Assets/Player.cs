9bfb95cae03ac8fee1a57ab7443c0eaa97635dc9
ichiYuuki <mrofushi@gmail.com>
2016-01-08 13:39:39 +0900

    バージョン2.0

---
+++ Unity-cahn move/Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 4f;
+	public GameObject mainCamera;
+	private Rigidbody2D rigidbody2D;
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		if (x != 0) {
+			rigidbody2D.velocity = new Vector2 (x * speed, rigidbody2D.velocity.y);
+			Vector2 temp = transform.localScale;
+			temp.x = x;
+			transform.localScale = temp;
+			anim.SetBool ("Dash",true);
+
+			if(transform.position.x > mainCamera.transform.position.x -4){
+				Vector3 cameraPos = mainCamera.transform.position;
+				cameraPos.x = transform.position.x + 4;
+				mainCamera.transform.position = cameraPos;
+			}
+			Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+			Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+			Vector2 pos = transform.position;
+			pos.x = Mathf.Clamp(pos.x, min.x + 0.5f, max.x);
+			transform.position = pos;
+
+		} else {
+			rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+			anim.SetBool("Dash",false);
+		}
+	}
+}
