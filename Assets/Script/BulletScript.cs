0e4d1b4c23d5a329781b05f273789c9befc2c7fe
ichiYuuki <mrofushi@gmail.com>
2016-01-10 16:49:42 +0900

    1/10ショット追加

---
+++ Assets/Script/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	private GameObject player;
+	private int speed = 10;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectsWithTag ("UnityChan");
+		Rigidbody2D rigidbody2D = GetComponent<Rigidbody2d>();
+		rigidbody2D.velocity = new Vector2 (speed * player.transform.localScale.x,rigidbody2D.velocity.y);
+		Vector2 temp = transform.localScale;
+		temp.x = player.transform.localScale.x;
+		transform.localScale = temp;
+		Destroy (gameObject,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
